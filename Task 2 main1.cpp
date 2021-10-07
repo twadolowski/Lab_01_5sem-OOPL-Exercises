dcad47e7d6ecd52e28ae0027f7a1af5d1f180960
Tomasz Wądołowski <56031022+twadolowski@users.noreply.github.com>
2021-10-07 12:30:31 +0200

    Add files via upload

---
+++ Task 2 main1.cpp
@@ -0,0 +1,37 @@
+// Task 2 main1.cpp
+#include <iostream>
+
+using namespace std;
+
+class Point
+{
+    float x,y;
+    
+public:
+
+Point(float x_ = 0, float y_ = 0)
+: x(x_), y(y_)
+
+{
+    
+}
+
+void set_x(float x_)
+{
+    x=x_;
+    
+void set_y(float y_)
+{
+    y=y_;
+    
+float get_x(x)
+{
+    return x;
+}
+
+float get_y(y)
+{
+    return y;
+}
+
+};
